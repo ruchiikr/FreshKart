3df742408f1c76f3837457be02e8a09a87a61f07
ruchi5286 <ruchikumari5286@gmail.com>
2025-08-12 20:37:01 +0530

    project completed

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ await connectDB();
 connectCloudinary();
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'https://fresh-kart-seven.vercel.app'];
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
