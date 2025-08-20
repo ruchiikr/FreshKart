ac9e1226f8ddc5056b161e8c9fd7355f840ccf15
Ruchi Kumari <142036407+ruchiikr@users.noreply.github.com>
2025-08-20 10:32:08 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -30,7 +30,7 @@ export const register = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -78,7 +78,7 @@ export const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+         sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -98,7 +98,8 @@ export const login = async (req, res) => {
 // Check Auth : /api/user/is-auth
 export const isAuth = async (req, res) => {
   try {
-    const { userId } = req.user; // ✅ fixed here
+    // const { userId } = req.user; // ✅ fixed here
+     const userId= req.user.id
     const user = await User.findById(userId).select("-password");
     
     if (!user) {
@@ -119,7 +120,7 @@ export const logout = async (req, res) => {
     res.clearCookie('token', {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        sameSite: 'none',
     });
 
     return res.json({ success: true, message: "Logged Out" });
